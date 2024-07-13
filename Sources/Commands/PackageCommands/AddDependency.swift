swiftlang/swift-package-manager

Fix swift package add-dependency for path dependency
Switched back to dependency Argument

Sources/Commands/PackageCommands/AddDependency.swift
@@ -52,31 +52,56 @@ extension SwiftPackageCommand {
 
         func run(_ swiftCommandState: SwiftCommandState) throws {
             let workspace = try swiftCommandState.getActiveWorkspace()
-
             guard let packagePath = try swiftCommandState.getWorkspaceRoot().packages.first else {
                 throw StringError("unknown package")
             }
 
-            // Load the manifest file
-            let fileSystem = workspace.fileSystem
-            let manifestPath = packagePath.appending("Package.swift")
-            let manifestContents: ByteString
-            do {
-                manifestContents = try fileSystem.readFileContents(manifestPath)
-            } catch {
-                throw StringError("cannot find package manifest in \(manifestPath)")
+            if let url = SourceControlURL(absoluteString: dependency) {
+                try self.createSourceControlPackage(
+                    packagePath: packagePath,
+                    workspace: workspace,
+                    url: url
+                )
+            } else if let path = try? Basics.AbsolutePath(validating: dependency) {
+                try self.createFileSystemPackage(
+                    packagePath: packagePath,
+                    workspace: workspace,
+                    path: path
+                )
+            } else if let _ = try? Basics.RelativePath(validating: dependency) {
+                throw StringError("relative paths not supported")
+            } else {
+                throw StringError("must provide valid url or path")
             }
+        }
 
-            // Parse the manifest.
-            let manifestSyntax = manifestContents.withData { data in
-                data.withUnsafeBytes { buffer in
-                    buffer.withMemoryRebound(to: UInt8.self) { buffer in
-                        Parser.parse(source: buffer)
-                    }
-                }
-            }
+        private func createFileSystemPackage(
+            packagePath: Basics.AbsolutePath,
+            workspace: Workspace,
+            path: Basics.AbsolutePath
+        ) throws {
+            let identity = PackageIdentity(path: path)
+            let packageDependency: PackageDependency = .fileSystem(
+                identity: identity,
+                nameForTargetDependencyResolutionOnly: nil,
+                path: path,
+                productFilter: .everything,
+                traits: []
+            )
 
-            let identity = PackageIdentity(url: .init(dependency))
+            try applyEdits(
+                packagePath: packagePath,
+                workspace: workspace,
+                packageDependency: packageDependency
+            )
+        }
+
+        private func createSourceControlPackage(
+            packagePath: Basics.AbsolutePath,
+            workspace: Workspace,
+            url: SourceControlURL
+        ) throws {
+            let identity = PackageIdentity(url: url)
 
             // Collect all of the possible version requirements.
             var requirements: [PackageDependency.SourceControl.Requirement] = []
@@ -123,22 +148,45 @@ extension SwiftPackageCommand {
                 }
             }
 
-            // Figure out the location of the package.
-            let location: PackageDependency.SourceControl.Location
-            if let path = try? Basics.AbsolutePath(validating: dependency) {
-                location = .local(path)
-            } else {
-                location = .remote(.init(dependency))
-            }
-
             let packageDependency: PackageDependency = .sourceControl(
                 identity: identity,
                 nameForTargetDependencyResolutionOnly: nil,
-                location: location,
+                location: .remote(url),
                 requirement: requirement,
                 productFilter: .everything,
                 traits: []
             )
+
+            try applyEdits(
+                packagePath: packagePath,
+                workspace: workspace,
+                packageDependency: packageDependency
+            )
+        }
+
+        private func applyEdits(
+            packagePath: Basics.AbsolutePath,
+            workspace: Workspace,
+            packageDependency: PackageDependency
+        ) throws {
+            // Load the manifest file
+            let fileSystem = workspace.fileSystem
+            let manifestPath = packagePath.appending(component: Manifest.filename)
+            let manifestContents: ByteString
+            do {
+                manifestContents = try fileSystem.readFileContents(manifestPath)
+            } catch {
+                throw StringError("cannot find package manifest in \(manifestPath)")
+            }
+
+            // Parse the manifest.
+            let manifestSyntax = manifestContents.withData { data in
+                data.withUnsafeBytes { buffer in
+                    buffer.withMemoryRebound(to: UInt8.self) { buffer in
+                        Parser.parse(source: buffer)
+                    }
+                }
+            }
 
             let editResult = try AddPackageDependency.addPackageDependency(
                 packageDependency,

Tests/CommandsTests/PackageCommandTests.swift
@@ -794,7 +794,7 @@ final class PackageCommandTests: CommandsTestCase {
         }
     }
 
-    func testPackageAddDependency() async throws {
+    func testPackageAddDependency_URL() async throws {
         try await testWithTemporaryDirectory { tmpPath in
             let fs = localFileSystem
             let path = tmpPath.appending("PackageB")
@@ -811,7 +811,15 @@ final class PackageCommandTests: CommandsTestCase {
                 """
             )
 
-            _ = try await execute(["add-dependency", "--branch", "main", "https://github.com/swiftlang/swift-syntax.git"], packagePath: path)
+            _ = try await execute(
+                [
+                    "add-dependency",
+                    "https://github.com/swiftlang/swift-syntax.git",
+                    "--branch",
+                    "main",
+                ],
+                packagePath: path
+            )
 
             let manifest = path.appending("Package.swift")
             XCTAssertFileExists(manifest)
@@ -820,6 +828,40 @@ final class PackageCommandTests: CommandsTestCase {
             XCTAssertMatch(contents, .contains(#".package(url: "https://github.com/swiftlang/swift-syntax.git", branch: "main"),"#))
         }
     }
+
+    func testPackageAddDependency_absolutePath() async throws {
+        try await testWithTemporaryDirectory { tmpPath in
+            let fs = localFileSystem
+            let path = tmpPath.appending("PackageB")
+            try fs.createDirectory(path)
+
+            try fs.writeFileContents(path.appending("Package.swift"), string:
+                """
+                // swift-tools-version: 5.9
+                import PackageDescription
+                let package = Package(
+                    name: "client",
+                    targets: [ .target(name: "client", dependencies: [ "library" ]) ]
+                )
+                """
+            )
+
+            _ = try await execute(
+                [
+                    "add-dependency",
+                    "/directory"
+                ],
+                packagePath: path
+            )
+
+            let manifest = path.appending("Package.swift")
+            XCTAssertFileExists(manifest)
+            let contents: String = try fs.readFileContents(manifest)
+
+            XCTAssertMatch(contents, .contains(#".package(path: "/directory"),"#))
+        }
+    }
+
 
     func testPackageAddTarget() async throws {
         try await testWithTemporaryDirectory { tmpPath in

hi2gage/swift-package-manager-fix-add-dependency-for-path

Sources/Commands/PackageCommands/AddDependency.swift
@@ -24,10 +24,14 @@ import Workspace
 extension SwiftPackageCommand {
     struct AddDependency: SwiftCommand {
         package static let configuration = CommandConfiguration(
-            abstract: "Add a package dependency to the manifest")
+            abstract: "Add a package dependency to the manifest"
+        )
 
-        @Argument(help: "The URL or directory of the package to add")
-        var dependency: String
+        @Option(help: "The URL of the package to add")
+        var url: String?
+
+        @Option(help: "The path of the package to add")
+        var path: String?
 
         @OptionGroup(visibility: .hidden)
         var globalOptions: GlobalOptions
@@ -51,32 +55,64 @@ extension SwiftPackageCommand {
         var to: Version?
 
         func run(_ swiftCommandState: SwiftCommandState) throws {
-            let workspace = try swiftCommandState.getActiveWorkspace()
+            guard url == nil || path == nil else {
+                throw StringError("cannot pass both url and directory")
+            }
 
+            guard !(url == nil && path == nil) else {
+                throw StringError("must specify --url or --directory")
+            }
+
+            let workspace = try swiftCommandState.getActiveWorkspace()
             guard let packagePath = try swiftCommandState.getWorkspaceRoot().packages.first else {
                 throw StringError("unknown package")
             }
 
-            // Load the manifest file
-            let fileSystem = workspace.fileSystem
-            let manifestPath = packagePath.appending("Package.swift")
-            let manifestContents: ByteString
-            do {
-                manifestContents = try fileSystem.readFileContents(manifestPath)
-            } catch {
-                throw StringError("cannot find package manifest in \(manifestPath)")
+            if let path {
+                try self.createFileSystemPackage(
+                    packagePath: packagePath,
+                    workspace: workspace,
+                    directory: path
+                )
+            } else if let url {
+                try self.createSourceControlPackage(
+                    packagePath: packagePath,
+                    workspace: workspace,
+                    url: url
+                )
             }
+        }
 
-            // Parse the manifest.
-            let manifestSyntax = manifestContents.withData { data in
-                data.withUnsafeBytes { buffer in
-                    buffer.withMemoryRebound(to: UInt8.self) { buffer in
-                        Parser.parse(source: buffer)
-                    }
-                }
+        private func createFileSystemPackage(
+            packagePath: Basics.AbsolutePath,
+            workspace: Workspace,
+            directory: String
+        ) throws {
+            guard let path = try? Basics.AbsolutePath(validating: directory) else {
+                throw StringError("Package path not found")
             }
+            let identity = PackageIdentity(path: path)
+            let packageDependency: PackageDependency = .fileSystem(
+                identity: identity,
+                nameForTargetDependencyResolutionOnly: nil,
+                path: path,
+                productFilter: .everything,
+                traits: []
+            )
 
-            let identity = PackageIdentity(url: .init(dependency))
+            try applyEdits(
+                packagePath: packagePath,
+                workspace: workspace,
+                packageDependency: packageDependency
+            )
+        }
+
+        private func createSourceControlPackage(
+            packagePath: Basics.AbsolutePath,
+            workspace: Workspace,
+            url: String
+        ) throws {
+            let identity = PackageIdentity(url: .init(url))
 
             // Collect all of the possible version requirements.
             var requirements: [PackageDependency.SourceControl.Requirement] = []
@@ -101,44 +137,71 @@ extension SwiftPackageCommand {
             }
 
             if requirements.count > 1 {
-                throw StringError("must specify at most one of --exact, --branch, --revision, --from, or --up-to-next-minor-from")
+                throw StringError(
+                    "must specify at most one of --exact, --branch, --revision, --from, or --up-to-next-minor-from"
+                )
             }
 
             guard let firstRequirement = requirements.first else {
-                throw StringError("must specify one of --exact, --branch, --revision, --from, or --up-to-next-minor-from")
+                throw StringError(
+                    "must specify one of --exact, --branch, --revision, --from, or --up-to-next-minor-from"
+                )
             }
 
             let requirement: PackageDependency.SourceControl.Requirement
             if case .range(let range) = firstRequirement {
                 if let to {
-                    requirement = .range(range.lowerBound..<to)
+                    requirement = .range(range.lowerBound ..< to)
                 } else {
                     requirement = .range(range)
                 }
             } else {
                 requirement = firstRequirement
 
-                if to != nil {
+                if self.to != nil {
                     throw StringError("--to can only be specified with --from or --up-to-next-minor-from")
                 }
-            }
-
-            // Figure out the location of the package.
-            let location: PackageDependency.SourceControl.Location
-            if let path = try? Basics.AbsolutePath(validating: dependency) {
-                location = .local(path)
-            } else {
-                location = .remote(.init(dependency))
             }
 
             let packageDependency: PackageDependency = .sourceControl(
                 identity: identity,
                 nameForTargetDependencyResolutionOnly: nil,
-                location: location,
+                location: .remote(.init(url)),
                 requirement: requirement,
                 productFilter: .everything,
                 traits: []
             )
+
+            try applyEdits(
+                packagePath: packagePath,
+                workspace: workspace,
+                packageDependency: packageDependency
+            )
+        }
+
+        private func applyEdits(
+            packagePath: Basics.AbsolutePath,
+            workspace: Workspace,
+            packageDependency: PackageDependency
+        ) throws {
+            // Load the manifest file
+            let fileSystem = workspace.fileSystem
+            let manifestPath = packagePath.appending(component: Manifest.filename)
+            let manifestContents: ByteString
+            do {
+                manifestContents = try fileSystem.readFileContents(manifestPath)
+            } catch {
+                throw StringError("cannot find package manifest in \(manifestPath)")
+            }
+
+            // Parse the manifest.
+            let manifestSyntax = manifestContents.withData { data in
+                data.withUnsafeBytes { buffer in
+                    buffer.withMemoryRebound(to: UInt8.self) { buffer in
+                        Parser.parse(source: buffer)
+                    }
+                }
+            }
 
             let editResult = try AddPackageDependency.addPackageDependency(
                 packageDependency,
@@ -149,7 +212,7 @@ extension SwiftPackageCommand {
                 to: fileSystem,
                 manifest: manifestSyntax,
                 manifestPath: manifestPath,
-                verbose: !globalOptions.logging.quiet
+                verbose: !self.globalOptions.logging.quiet
             )
         }
     }

Tests/CommandsTests/PackageCommandTests.swift
@@ -794,7 +794,7 @@ final class PackageCommandTests: CommandsTestCase {
         }
     }
 
-    func testPackageAddDependency() async throws {
+    func testPackageAddURLDependency() async throws {
         try await testWithTemporaryDirectory { tmpPath in
             let fs = localFileSystem
             let path = tmpPath.appending("PackageB")
@@ -811,7 +811,16 @@ final class PackageCommandTests: CommandsTestCase {
                 """
             )
 
-            _ = try await execute(["add-dependency", "--branch", "main", "https://github.com/swiftlang/swift-syntax.git"], packagePath: path)
+            _ = try await execute(
+                [
+                    "add-dependency",
+                    "--url",
+                    "https://github.com/swiftlang/swift-syntax.git",
+                    "--branch",
+                    "main",
+                ],
+                packagePath: path
+            )
 
             let manifest = path.appending("Package.swift")
             XCTAssertFileExists(manifest)
@@ -820,6 +829,41 @@ final class PackageCommandTests: CommandsTestCase {
             XCTAssertMatch(contents, .contains(#".package(url: "https://github.com/swiftlang/swift-syntax.git", branch: "main"),"#))
         }
     }
+
+    func testPackageAddPathDependency() async throws {
+        try await testWithTemporaryDirectory { tmpPath in
+            let fs = localFileSystem
+            let path = tmpPath.appending("PackageB")
+            try fs.createDirectory(path)
+
+            try fs.writeFileContents(path.appending("Package.swift"), string:
+                """
+                // swift-tools-version: 5.9
+                import PackageDescription
+                let package = Package(
+                    name: "client",
+                    targets: [ .target(name: "client", dependencies: [ "library" ]) ]
+                )
+                """
+            )
+
+            _ = try await execute(
+                [
+                    "add-dependency",
+                    "--path",
+                    "/directory"
+                ],
+                packagePath: path
+            )
+
+            let manifest = path.appending("Package.swift")
+            XCTAssertFileExists(manifest)
+            let contents: String = try fs.readFileContents(manifest)
+
+            XCTAssertMatch(contents, .contains(#".package(path: "/directory"),"#))
+        }
+    }
+
 
     func testPackageAddTarget() async throws {
         try await testWithTemporaryDirectory { tmpPath in